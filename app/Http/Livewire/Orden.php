658d23bb1a94e63dd53f2bc16ccaa15b33534875
arsagolu342 <arielgordillolucas@gmail.com>
2022-03-03 10:17:25 -0500

    sd

---
+++ app/Http/Livewire/Orden.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Order;
+
+use App\Models\Order_Detail;
+
+
+class Orden extends Component
+{
+    public $search = '';
+    public $precio = '';
+    public $fecha = '';
+    public $id_ord;
+    public $accion = 'store';
+
+
+  public $tipo_envio = '';
+    public $direccion_envio = '';
+       public function render()
+    {
+        $order = Order::where('fecha','like','%'. $this->search . '%')->get();
+        return view('livewire.orden',compact('order'));
+    }
+    public function store()
+    {
+        // $this->accion = "store";
+
+        Order::create([
+            'precio' => $this->precio,
+            'fecha' => $this->fecha,
+            
+            
+
+        ]);
+
+        Order_Detail::create([
+            'tipo_envio' => $this->tipo_envio,
+            'direccion_envio' => $this->direccion_envio,
+        ]);
+        $this->reset(['precio', 'fecha', 'tipo_envio', 'direccion_envio']);
+    }
+
+    //Edit Corresponsales
+    public function edit(Order $corr)
+    {
+        $this->accion = "update";
+        $this->precio = $corr->precio;
+        $this->fecha= $corr->fecha;
+        $this->id_ord = $corr->id;
+    }
+
+
+    public function update()
+    {
+        $this->validate();
+        $corr = Order::find($this->id_ord);
+        $corr->update([
+            'precio' => $this->precio,
+            'fecha' => $this->fecha,
+            
+        ]);
+        $this->reset(['precio', 'fecha']);
+
+
+    }
+    function
+default() {
+    $this->reset(['precio', 'fecha']);
+
+    }
+    public function destroy(Order $corr)
+    {
+        $corr->delete();
+    }
+}
